a5cee575d13f84a13d790a2c6566113bedb0eb99
mattleh <matteo.lehner@gmail.com>
2023-05-18 12:44:27 +0200

    added data vom hydro.ooe.gv

--- pegel.py
+++ pegel.py
@@ -9,6 +9,7 @@ import paho.mqtt.client as MQTT
 import os
 from sys import stdout
 import atexit
+from urllib.request import urlopen
 
 logging.basicConfig(level=logging.INFO)
 
@@ -112,14 +113,95 @@ while True:
           mqtt.username_pw_set(mqtt_user, mqtt_password)
       mqtt.connect(mqtt_server, mqtt_port)
 
+  stationdata = []
+
   for nr, measurement in data.items():
-  #  mqtt.publish("jarvis/water_level/"+nr, json.dumps(measurement, ensure_ascii=False))
+    urls = {f"https://hydro.ooe.gv.at/daten/internet/stations/OG/{nr}/S/alm.json",
+        f"https://hydro.ooe.gv.at/daten/internet/stations/OG/{nr}/S/events.json",
+        f"https://hydro.ooe.gv.at/daten/internet/stations/OG/{nr}/S/ltv.json"
+    }
+  
+    for url in urls:
+        stationresp = urlopen(url)
+        stationdata.append(json.loads(response.read()))
+
+        for set in data:
+          for entry in set:
+              id = entry.get("station_no")
+              shortname = entry.get("ts_shortname").split(".")[1]
+              data[id][shortname] = entry.get("data")[-1][1]
+
     name = measurement.get("location") + "-" + measurement.get("water")
     unit = measurement.get("unit")
     level = measurement.get("value")
     water = measurement.get("water")
     location = measurement.get("location")
     timest = measurement.get("timestamp")
+
+    if data[nr].get("Voralarm"):
+      voralarm = data[nr].get("Voralarm")
+    else:
+      voralarm = ""
+
+    if data[nr].get("Alarm1"):
+      alarm1 = data[nr].get("Alarm1")
+    else:
+      alarm1 = ""
+
+    if data[nr].get("Alarm2"):
+      alarm2 = data[nr].get("Alarm2")
+    else:
+      alarm2 = ""
+
+    if data[nr].get("Alarm3"):
+      alarm3 = data[nr].get("Alarm3")
+    else:
+      alarm3 = ""
+
+    if data[nr].get("Event"):
+      event = data[nr].get("Event")
+    else:
+      event = ""
+
+    if data[nr].get("HW1"):
+      hw1 = data[nr].get("HW1")
+    else:
+      hw1 = ""
+
+    if data[nr].get("HW2"):
+      hw2 = data[nr].get("HW2")
+    else:
+      hw2 = ""
+
+    if data[nr].get("HW5"):
+      hw5 = data[nr].get("HW5")
+    else:
+      hw5 = ""
+
+    if data[nr].get("HW10"):
+      hw10 = data[nr].get("HW10")
+    else:
+      hw10 = ""
+
+    if data[nr].get("HW30"):
+      hw30 = data[nr].get("HW30")
+    else:
+      hw30 = ""
+
+    if data[nr].get("HW100"):
+      hw100 = data[nr].get("HW100")
+    else:
+      hw100 = ""      
+
+    if data[nr].get("NW"):
+      nw = data[nr].get("NW")
+    else:
+      nw = ""
+
+    if data[nr].get("MW"):
+      mw = data[nr].get("MW")
+    else:
+      mw = ""
 
     autodiscover= mqtt.publish(
         f"homeassistant/sensor/pegel_bridge/{nr}/config", 
@@ -130,7 +212,6 @@ while True:
                 "json_attributes_topic": f"homeassistant/sensor/pegel_{nr}/attr",
                 "unit_of_measurement": unit,
                 "value_template": "{{ value_json.level }}",
-                "json_attributes_template": "{{ value_json.timestamp | timestamp_local }}",
                 "device": {
                     "identifiers": ["pegel_bridge"],
                     "manufacturer": "ML/LH",
@@ -152,6 +233,19 @@ while True:
                 "Water": water,
                 "Location": location,
                 "timestamp": timest,
+                "Voralarm": voralarm,
+                "Alarm1": alarm1,
+                "Alarm2": alarm2,
+                "Alarm3": alarm3,
+                "Last_Event": event,
+                "HW1": hw1,
+                "HW2": hw2,
+                "HW5": hw5,
+                "HW10": hw10,
+                "HW30": hw30,
+                "HW100": hw100,
+                "Niederwasser": nw,
+                "Mittelwasser": mw,
             }
         ),
     )
