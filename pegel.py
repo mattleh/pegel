a1a63aa67974026f152c836d5cc0e78919b0d280
mattleh <matteo.lehner@gmail.com>
2023-05-24 16:21:41 +0200

    mep

--- pegel.py
+++ pegel.py
@@ -55,68 +55,68 @@ def exit_handler():
     mqtt.loop_stop()
 
 atexit.register(exit_handler)
-#%%
-while True:
-  # %%
+
+# %% get pegel data
+def get_pegel():
   # daten abholen
-  response = requests.get('http://data.ooe.gv.at/files/hydro/HDOOE_Export_OG.zrxp')
+    response = requests.get('http://data.ooe.gv.at/files/hydro/HDOOE_Export_OG.zrxp')
 
-  data = {}
-  nr = None
-  # über alle Zeilen iterieren. Die Response ist so aufgebaut, dass immer zuerst
-  # der Header kommt und anschließend die zugehörigen Datenzeilen
-  for line in response.iter_lines():
-    # Headerzeilen parsen
+    data = {}
+    nr = None
+    # über alle Zeilen iterieren. Die Response ist so aufgebaut, dass immer zuerst
+    # der Header kommt und anschließend die zugehörigen Datenzeilen
+    for line in response.iter_lines():
+      # Headerzeilen parsen
 
-    if line.decode('latin1').startswith('#') and '|' in line.decode('latin1'):
-      # erstes Zeichen wegschnippseln und nach | trennen und iterieren
-      line = line[1:]
-      elements = line.split(b'|')
-      for element in elements:
-        # manche Datenelemente sind UTF-8 kodiert, andere LATIN1, und andere einfach nur Müll ;-(
-        try:
-          element = element.decode('utf-8', 'strict')
-        except UnicodeDecodeError:
-         element = element.decode('latin1', 'strict')
-        # ID parsen
-        if element.startswith('SANR'):
-          nr = element.encode('utf-8')[4:].decode('utf-8')
-          data[nr] = { 'unique_id' : nr }
-        # Gewässer parsen
-        if element.startswith('SWATER'):
-          data[nr]['water'] = element.encode('utf-8')[6:].decode('utf-8')
-        # Stationsname parsen
-        if element.startswith('SNAME'):
-          data[nr]['location'] = element.encode('utf-8')[5:].decode('utf-8')
-        # Unit parsen
-        if element.startswith('CUNIT'):
-          data[nr]['unit'] = element.encode('utf-8')[5:].decode('utf-8')
-        # Timezone parsen
-        if element.startswith('TZ'):
-          data[nr]['tz'] = element.encode('utf-8')[2:].decode('utf-8')
-    else:
-      new_entry = True
-      if nr == None or len(line) == 0:
-        continue
-      line = line.decode('latin1')
-      elements = line.split(' ')
-      # erstes Element: Timecode parsen und in Unix-Timestamp verwandeln
-      #timestamp = int(datetime.datetime.strptime(elements[0], "%Y%m%d%H%M%S").timestamp())
-      timestamp = parser.parse(f'{elements[0]} {data[nr].get("tz")[3:]}')
-      # zweites Element: Höhe in cm, allerdings gibt es ungültige Messwerte mit negativen Werten
-      value = int(elements[1])
-      # Timestamp und Value ersetzen, wenn es einen aktuelleren, gültigen Wert in der aktuellen Zeile gibt
-      if ('timestamp' not in data[nr] or timestamp > data[nr]['timestamp']) and value >= 0:
-        data[nr]['timestamp'] = timestamp
-        data[nr]['value'] = value
+      if line.decode('latin1').startswith('#') and '|' in line.decode('latin1'):
+        # erstes Zeichen wegschnippseln und nach | trennen und iterieren
+        line = line[1:]
+        elements = line.split(b'|')
+        for element in elements:
+          # manche Datenelemente sind UTF-8 kodiert, andere LATIN1, und andere einfach nur Müll ;-(
+          try:
+            element = element.decode('utf-8', 'strict')
+          except UnicodeDecodeError:
+            element = element.decode('latin1', 'strict')
+          # ID parsen
+          if element.startswith('SANR'):
+            nr = element.encode('utf-8')[4:].decode('utf-8')
+            data[nr] = { 'unique_id' : nr }
+          # Gewässer parsen
+          if element.startswith('SWATER'):
+            data[nr]['water'] = element.encode('utf-8')[6:].decode('utf-8')
+          # Stationsname parsen
+          if element.startswith('SNAME'):
+            data[nr]['location'] = element.encode('utf-8')[5:].decode('utf-8')
+          # Unit parsen
+          if element.startswith('CUNIT'):
+            data[nr]['unit'] = element.encode('utf-8')[5:].decode('utf-8')
+          # Timezone parsen
+          if element.startswith('TZ'):
+            data[nr]['tz'] = element.encode('utf-8')[2:].decode('utf-8')
+      else:
+        new_entry = True
+        if nr == None or len(line) == 0:
+          continue
+        line = line.decode('latin1')
+        elements = line.split(' ')
+        # erstes Element: Timecode parsen und in Unix-Timestamp verwandeln
+        #timestamp = int(datetime.datetime.strptime(elements[0], "%Y%m%d%H%M%S").timestamp())
+        timestamp = parser.parse(f'{elements[0]} {data[nr].get("tz")[3:]}')
+        # zweites Element: Höhe in cm, allerdings gibt es ungültige Messwerte mit negativen Werten
+        value = int(elements[1])
+        # Timestamp und Value ersetzen, wenn es einen aktuelleren, gültigen Wert in der aktuellen Zeile gibt
+        if ('timestamp' not in data[nr] or timestamp > data[nr]['timestamp']) and value >= 0:
+          data[nr]['timestamp'] = timestamp
+          data[nr]['value'] = value
 
-  # Korrektur für kaputte Daten die doppelt transkodiert wurden -> Müll
-  data['9450']['location'] = 'Unterweißenbach'
-  data['5230']['location'] = 'Weißenbach am Attersee'
-  data['8445']['location'] = 'Roßleithen'
-  
-  # %%
-  for nr, measurement in data.items():
+    # Korrektur für kaputte Daten die doppelt transkodiert wurden -> Müll
+    data['9450']['location'] = 'Unterweißenbach'
+    data['5230']['location'] = 'Weißenbach am Attersee'
+    data['8445']['location'] = 'Roßleithen'
+    
+    for item in data.items():
+      nr = item[0]
       # get and add additional data from web
       urls = {f"https://hydro.ooe.gv.at/daten/internet/stations/OG/{nr}/S/alm.json",
       f"https://hydro.ooe.gv.at/daten/internet/stations/OG/{nr}/S/events.json",
@@ -135,13 +135,16 @@ while True:
                 data[nr][shortname] = entry.get("data")[-1][1]
               else:
                 data[nr][shortname] = ''  
+    
+    return data
 
-      # publish config to mqtt HA
-      mqtt.publish(
+def publish_mqtt(nr):
+    # publish config to mqtt HA
+    mqtt.publish(
           f"homeassistant/sensor/pegel_bridge/{nr}/config", 
           json.dumps(
               {
-                  "name": measurement.get("location") + "-" + measurement.get("water"),
+                  "name": f'{data[nr]["location"]} {data[nr]["water"]}',
                   "state_topic": f"homeassistant/sensor/pegel_{nr}/state",
                   "json_attributes_topic": f"homeassistant/sensor/pegel_{nr}/attr",
                   "unit_of_measurement": data[nr]['unit'],
@@ -156,13 +159,13 @@ while True:
               }
           ),
       )
-      # publish value to mqtt HA
-      mqtt.publish(
+    # publish value to mqtt HA
+    mqtt.publish(
           f"homeassistant/sensor/pegel_{nr}/state",
           json.dumps({"level": data[nr]['value']}),
       )
-      # publish additional data to mqtt HA
-      mqtt.publish(
+    # publish additional data to mqtt HA
+    mqtt.publish(
           f"homeassistant/sensor/pegel_{nr}/attr",
           json.dumps(
               {
@@ -185,10 +188,15 @@ while True:
               }
           ),
       )
-  
+#%%
+while True:
+  # %%
+  data = get_pegel()
+
+  # %%
+  for nr in data.items():
+      publish_mqtt(nr)
   print('Pegel Sendt')
-  data.clear()
-  #stationdata.clear()
   gc.collect()
   # ein wenig schlafen
   time.sleep(60*sleep)
