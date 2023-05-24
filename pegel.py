cee6fe0d28bb049dd58e498ae5342169f3ccc993
mattleh <matteo.lehner@gmail.com>
2023-05-24 11:49:44 +0200

    cleanup. and some tweaking

--- pegel.py
+++ pegel.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-
+# %% import
 import logging
 import requests
 import datetime
+from dateutil import parser
 import json
 import time
 import paho.mqtt.client as MQTT
@@ -11,6 +12,7 @@ from sys import stdout
 import atexit
 from urllib.request import urlopen
 
+# %%
 logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
@@ -48,19 +50,40 @@ def exit_handler():
 atexit.register(exit_handler)
 
 while True:
-
+  # %%
   # daten abholen
   response = requests.get('http://data.ooe.gv.at/files/hydro/HDOOE_Export_OG.zrxp')
 
   data = {}
   nr = None
-
+  new_entry = False
   # über alle Zeilen iterieren. Die Response ist so aufgebaut, dass immer zuerst
   # der Header kommt und anschließend die zugehörigen Datenzeilen
   for line in response.iter_lines():
     # Headerzeilen parsen
+    
     if line.decode('latin1').startswith('#') and '|' in line.decode('latin1'):
       # erstes Zeichen wegschnippseln und nach | trennen und iterieren
+      if new_entry:
+        urls = {f"https://hydro.ooe.gv.at/daten/internet/stations/OG/{nr}/S/alm.json",
+        f"https://hydro.ooe.gv.at/daten/internet/stations/OG/{nr}/S/events.json",
+        f"https://hydro.ooe.gv.at/daten/internet/stations/OG/{nr}/S/ltv.json"
+    }
+        for url in urls:
+          stationdata = []
+          stationresp = urlopen(url)
+          stationdata.append(json.loads(stationresp.read()))
+
+          for set in stationdata:
+            for entry in set:
+                id = entry.get("station_no")
+                shortname = entry.get("ts_shortname").split(".")[1]
+                if len(entry.get("data")) > 0:
+                  data[id][shortname] = entry.get("data")[-1][1]
+                else:
+                  data[id][shortname] = ''  
+
+      new_entry = False
       line = line[1:]
       elements = line.split(b'|')
       for element in elements:
@@ -85,15 +108,15 @@ while True:
         # Timezone parsen
         if element.startswith('TZ'):
           data[nr]['tz'] = element.encode('utf-8')[2:].decode('utf-8')
-
-    # Datenzeilen parsen jeweils Zeitstempel und Wert dazu mit Leerzeichen getrennt
-    if line.decode('latin1').startswith('20'):
-      if nr == None:
+    else:
+      new_entry = True
+      if nr == None or len(line) == 0:
         continue
       line = line.decode('latin1')
       elements = line.split(' ')
       # erstes Element: Timecode parsen und in Unix-Timestamp verwandeln
-      timestamp = int(datetime.datetime.strptime(elements[0], "%Y%m%d%H%M%S").timestamp())
+      #timestamp = int(datetime.datetime.strptime(elements[0], "%Y%m%d%H%M%S").timestamp())
+      timestamp = parser.parse(f'{elements[0]} {data[nr].get("tz")}')
       # zweites Element: Höhe in cm, allerdings gibt es ungültige Messwerte mit negativen Werten
       value = int(elements[1])
       # Timestamp und Value ersetzen, wenn es einen aktuelleren, gültigen Wert in der aktuellen Zeile gibt
@@ -101,119 +124,27 @@ while True:
         data[nr]['timestamp'] = timestamp
         data[nr]['value'] = value
 
-
   # Korrektur für kaputte Daten die doppelt transkodiert wurden -> Müll
   data['9450']['location'] = 'Unterweißenbach'
   data['5230']['location'] = 'Weißenbach am Attersee'
   data['8445']['location'] = 'Roßleithen'
   
+  # %%
   if flag_connected == 0:
       print("connecting mqtt")  
       if mqtt_user and mqtt_password:
           mqtt.username_pw_set(mqtt_user, mqtt_password)
       mqtt.connect(mqtt_server, mqtt_port)
 
-  stationdata = []
-
   for nr, measurement in data.items():
-    urls = {f"https://hydro.ooe.gv.at/daten/internet/stations/OG/{nr}/S/alm.json",
-        f"https://hydro.ooe.gv.at/daten/internet/stations/OG/{nr}/S/events.json",
-        f"https://hydro.ooe.gv.at/daten/internet/stations/OG/{nr}/S/ltv.json"
-    }
-  
-    name = measurement.get("location") + "-" + measurement.get("water")
-    unit = measurement.get("unit")
-    level = measurement.get("value")
-    water = measurement.get("water")
-    location = measurement.get("location")
-    timest = measurement.get("timestamp")
-
-    for url in urls:
-        stationresp = urlopen(url)
-        stationdata.append(json.loads(stationresp.read()))
-
-        for set in stationdata:
-          for entry in set:
-              id = entry.get("station_no")
-              shortname = entry.get("ts_shortname").split(".")[1]
-              try:
-                data[id][shortname] = entry.get("data")[-1][1]
-              except:
-                pass
-
-    if data[nr].get("Voralarm"):
-      voralarm = data[nr].get("Voralarm")
-    else:
-      voralarm = ""
-
-    if data[nr].get("Alarm1"):
-      alarm1 = data[nr].get("Alarm1")
-    else:
-      alarm1 = ""
-
-    if data[nr].get("Alarm2"):
-      alarm2 = data[nr].get("Alarm2")
-    else:
-      alarm2 = ""
-
-    if data[nr].get("Alarm3"):
-      alarm3 = data[nr].get("Alarm3")
-    else:
-      alarm3 = ""
-
-    if data[nr].get("Event"):
-      event = data[nr].get("Event")
-    else:
-      event = ""
-
-    if data[nr].get("HW1"):
-      hw1 = data[nr].get("HW1")
-    else:
-      hw1 = ""
-
-    if data[nr].get("HW2"):
-      hw2 = data[nr].get("HW2")
-    else:
-      hw2 = ""
-
-    if data[nr].get("HW5"):
-      hw5 = data[nr].get("HW5")
-    else:
-      hw5 = ""
-
-    if data[nr].get("HW10"):
-      hw10 = data[nr].get("HW10")
-    else:
-      hw10 = ""
-
-    if data[nr].get("HW30"):
-      hw30 = data[nr].get("HW30")
-    else:
-      hw30 = ""
-
-    if data[nr].get("HW100"):
-      hw100 = data[nr].get("HW100")
-    else:
-      hw100 = ""      
-
-    if data[nr].get("NW"):
-      nw = data[nr].get("NW")
-    else:
-      nw = ""
-
-    if data[nr].get("MW"):
-      mw = data[nr].get("MW")
-    else:
-      mw = ""
-
     mqtt.publish(
         f"homeassistant/sensor/pegel_bridge/{nr}/config", 
         json.dumps(
             {
-                "name": name,
+                "name": measurement.get("location") + "-" + measurement.get("water"),
                 "state_topic": f"homeassistant/sensor/pegel_{nr}/state",
                 "json_attributes_topic": f"homeassistant/sensor/pegel_{nr}/attr",
-                "unit_of_measurement": unit,
+                "unit_of_measurement": data[nr]['unit'],
                 "value_template": "{{ value_json.level }}",
                 "device": {
                     "identifiers": ["pegel_bridge"],
@@ -228,29 +159,29 @@ while True:
   
     mqtt.publish(
         f"homeassistant/sensor/pegel_{nr}/state",
-        json.dumps({"level": level}),
+        json.dumps({"level": data[nr]['level']}),
     )
   
     mqtt.publish(
         f"homeassistant/sensor/pegel_{nr}/attr",
         json.dumps(
             {
-                "Water": water,
-                "Location": location,
-                "timestamp": timest,
-                "Voralarm": voralarm,
-                "Alarm1": alarm1,
-                "Alarm2": alarm2,
-                "Alarm3": alarm3,
-                "Last_Event": event,
-                "HW1": hw1,
-                "HW2": hw2,
-                "HW5": hw5,
-                "HW10": hw10,
-                "HW30": hw30,
-                "HW100": hw100,
-                "Niederwasser": nw,
-                "Mittelwasser": mw,
+                "Water": data[nr]['water'],
+                "Location": data[nr]['location'],
+                "timestamp": data[nr]['timestamp'],
+                "Voralarm": data[nr]['Voralarm'],
+                "Alarm1": data[nr]['Alarm1'],
+                "Alarm2": data[nr]['Alarm2'],
+                "Alarm3": data[nr]['Alarm3'],
+                "Last_Event": data[nr]['Event'],
+                "HW1": data[nr]['HW1'],
+                "HW2": data[nr]['HW2'],
+                "HW5": data[nr]['HW5'],
+                "HW10": data[nr]['HW10'],
+                "HW30": data[nr]['HW30'],
+                "HW100": data[nr]['HW100'],
+                "Niederwasser": data[nr]['NW'],
+                "Mittelwasser": data[nr]['MW'],
             }
         ),
     )
