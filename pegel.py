15a12c915b99dc71622f027729cc938f51791792
mattleh <matteo.lehner@gmail.com>
2023-11-15 15:39:23 +0100

    x

--- pegel.py
+++ pegel.py
@@ -220,7 +220,7 @@ while True:
   start = time.time()
   data = get_pegel()
   end = time.time()
-  print(f"Took {end - start}s collecting data')
+  print(f"Took {end - start}s collecting data")
   # %%
   for nr in data.items():
       publish_mqtt(nr)
